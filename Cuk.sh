28796ceaf51fa82dd93f2b558238b95e8a9062d7
⎝◣⏝◢⎠SANTRI-CYBER-SANTUY⎝◣⏝◢⎠ <79558956+AKKUSANTUY@users.noreply.github.com>
2021-04-05 14:11:54 +0700

    Create Cuk.sh

---
+++ Cuk.sh
@@ -0,0 +1,82 @@
+# Prank link github
+# JANGAN RECODE ANJIM
+# LU MAMPU NGA BOSS
+
+clear
+echo ""
+echo "Baru Belajar Buat Script Cuk"
+echo ""
+echo "Maklumi lah gan gw masih dalam proses belajar"
+echo ""
+echo "Ga suka Cuk"
+echo ""
+echo "Berantem Hayyuuk"
+echo ""
+echo "Awowkwowk Canda :v"
+echo ""
+sleep 3
+clear
+toilet -f big -F gay SANTRI
+sleep 1
+toilet -f big -F gay CYBER
+sleep 1
+toilet -f big -F gay SANTUY
+sleep 1
+toilet -f big -F gay TOOLS PRK
+#sleep 1
+echo "===================================="
+sleep 1
+echo " Author    : SANTRI CYBER SANTUY"
+sleep 1
+echo " Github    : https://github.com/AKKUSANTUY"
+sleep 1
+echo " Instagram : AKKU_SANTUY"
+sleep 1
+echo " WhatsApp  : +62 821-3456-6596"
+sleep 1
+echo "===================================="
+echo ""
+echo ""
+sleep 1
+echo "================================
+echo " [!] Dipilih Tools Nya Cuk [!] "
+echo "================================
+echo "[1] DarkFb"
+echo "[2] LiteDdos"
+echo "[3] Trojan Nomor Hp"
+echo "[4] Download Bahannya Dulu Cuk"
+echo "================================
+read -p "[?] Pilih Cuk : " pil :
+# Batas gan
+if [ $pil = 1 ]
+then
+git clone https://github.com/4DM1NH/DarkFbv2
+cd DarkFbv2
+python2 D4RKFBv2.py
+fi
+# Batas gan
+if [ $pil = 2 ]
+then
+git clone https://github.com/4L13199/LITEDDOS
+cd LITEDDOS
+python2 LITEDDOS.py
+fi
+# Batas gan
+if [ $pil = 3 ]
+then
+git clone https://github.com/R133F/Trojans
+cd Trojans
+python2 trojans.py
+fi
+# Batas gan
+if [ $pil = 4 ]
+then
+apt update && apt upgrade
+pkg install nano
+pkg install python
+pkg install php
+pkg install git
+pkg install python2
+pip install requests mechanize
+pip2 install requests mechanize
+fi
